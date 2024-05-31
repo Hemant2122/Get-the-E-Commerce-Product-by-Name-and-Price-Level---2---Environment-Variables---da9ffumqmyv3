18341deb703b83f0ef5cc5dacab06959692e482b
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-05-31 21:40:53 +0530

    Get the E-Commerce Product level - 2

--- src/app.js
+++ src/app.js
@@ -11,4 +11,26 @@ app.use(express.json());
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
 
+app.get("/api/v1/products/:id", (req, res) => {
+    const id = Number(req.params["id"]);
+    const product = products.find((product) => product.id === id)
+    console.log(product);
+    if(!product){
+        req.statusCode = 404;
+        const output = { "status": "failed", "message": "Product not found!" };
+        res.send(output);
+    }else{
+        req.statusCode = 200;
+        const output = {
+            status: "success", 
+            message: "Product fetched successfully",
+            data: {
+                product,
+            }
+
+        }
+        res.send(output);
+    }
+});
+
 module.exports = app;
